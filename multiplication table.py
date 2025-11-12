97de50901936b6d9eb6d931d08003261d765297f
vinay kumar meti 89 <vinaykumarmeti.2006@gmail.com>
2025-11-12 10:35:53 +0530

    multiplication table.py

---
+++ multiplication table.py
@@ -0,0 +1,14 @@
+number=int(input("enter a number"))
+for i in range(1,11):
+ print(f"{number} x {i} = {number*i}")
+enter a number3
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
