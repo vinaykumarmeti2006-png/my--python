0d273d1fd816bc876bfc99880f5464e32d996c7b
vinay kumar meti 89 <vinaykumarmeti.2006@gmail.com>
2025-11-13 09:11:21 +0530

    demonstration of pass statement in loop.py

---
+++ to demonstrate pass statement.py
@@ -0,0 +1,17 @@
+for i in range(1,11):
+  if i==5:
+     pass
+  print("current number:",i)
+print("loop finished")
+current number: 1
+current number: 2
+current number: 3
+current number: 4
+current number: 5
+current number: 6
+current number: 7
+current number: 8
+current number: 9
+current number: 10
+loop finished
+
