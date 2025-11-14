02f5e7ef540f044a9d676109577bf9cbf2685317
vinay kumar meti 89 <vinaykumarmeti.2006@gmail.com>
2025-11-14 23:33:39 +0530

    Create List of 5 numbers entered by user.py

---
+++ List of 5 numbers entered by user.py
@@ -0,0 +1,16 @@
+#to create a list of 5 number entered by the user and print the list
+numbers=[]
+for i in range(4):
+  n=int(input("enter the number:"))
+  numbers.append(n)
+
+  print(numbers)
+enter the number:1
+[1]
+enter the number:2
+[1, 2]
+enter the number:3
+[1, 2, 3]
+enter the number:4
+[1, 2, 3, 4]
+
