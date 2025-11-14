f815b2502293de4ba19cbb9ee9c7e5a184ae3d69
vinay kumar meti 89 <vinaykumarmeti.2006@gmail.com>
2025-11-14 23:25:26 +0530

    Create List .py

---
+++ List .py
@@ -0,0 +1,21 @@
+a = [1,2,3,4,5,6]
+print(a[4])
+print(a[-3])
+a.append(34)
+print(a)
+a.insert(1,20)
+a.remove(5)
+print(a)
+a.pop(3)
+print(a)
+a.sort()
+print(a)
+a.reverse
+print(a)
+5
+4
+[1, 2, 3, 4, 5, 6, 34]
+[1, 20, 2, 3, 4, 6, 34]
+[1, 20, 2, 4, 6, 34]
+[1, 2, 4, 6, 20, 34]
+[1, 2, 4, 6, 20, 34]
