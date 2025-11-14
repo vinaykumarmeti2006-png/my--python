8b52404a2b6641b50b01c44c4503c06f1b5a6778
vinay kumar meti 89 <vinaykumarmeti.2006@gmail.com>
2025-11-14 22:59:37 +0530

    addition.py with user input for calculate.py
    Implement addition and multiplication functions with user input.

---
+++ Addition.py
@@ -0,0 +1,18 @@
+
+def addition(x,y):
+  name  = input("enter your name:")
+  age   = int(input("enter your age:"))
+  rollno = int(input("enter your rollno:"))
+  addition = age+rollno
+  print("the sum is: ",addition)
+  mul = age+rollno
+  print("mul is: ",mul)
+  return name,age,rollno
+  print(a,b,c,sum)
+addition(1,9)
+enter your name:Vinay 
+enter your age:18
+enter your rollno:47
+the sum is:  65
+mul is:  65
+('Vinay ', 18, 47)
